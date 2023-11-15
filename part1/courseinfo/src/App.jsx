084289cb91b181574a28734dd3c1335fe86e44bf
MooCodes <60076255+MooCodes@users.noreply.github.com>
2023-11-15 05:35:33 -0600

    finished exercise 1.4

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -25,7 +25,7 @@ const Part = (props) => {
 }
 
 const Total = (props) => {
-  const total = props.courseData.reduce((acc, curr) => acc + curr, 0)
+  const total = props.courseData.reduce((acc, curr) => acc + curr.exercises, 0)
   return (
     <div>
       <p>Number of exercises {total}</p>
@@ -35,24 +35,26 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header name={course} />
-      <Content courseData={[part1, part2, part3]}/>
-      <Total courseData={[part1.exercises, part2.exercises, part3.exercises]} />
+      <Content courseData={parts}/>
+      <Total courseData={parts} />
     </div>
   )
 }

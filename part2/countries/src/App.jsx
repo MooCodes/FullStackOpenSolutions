5de375ec1574807a0fedfa4b8c1b5f0e7b3f368f
MooCodes <60076255+MooCodes@users.noreply.github.com>
2023-11-19 15:41:26 -0600

    finished exercise 2.18

---
+++ part2/countries/src/App.jsx
@@ -0,0 +1,90 @@
+import { useEffect, useState } from "react";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [apiData, setApiData] = useState(null);
+  const [results, setResults] = useState(null);
+
+  // fetch the api data only on the first render and set it to the current api data state
+  useEffect(() => {
+    const url = "https://studies.cs.helsinki.fi/restcountries/api/all";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setApiData(data);
+      });
+  }, []);
+
+  // whenever searchTerm is changed, update the results with the new search term
+  useEffect(() => {
+    if (apiData) {
+      // if it's empty, don't reset the results state back to empty state
+      if (searchTerm === "") return setResults(null);
+
+      const filteredResults = apiData.filter((data) =>
+        data.name.common.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      console.log(filteredResults);
+      setResults(filteredResults);
+    }
+  }, [searchTerm]);
+
+  // if api data has not been loaded yet, don't render anything
+  if (!apiData) return null;
+
+  return (
+    <div>
+      find countries{" "}
+      <input
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <Results results={results} />
+    </div>
+  );
+};
+
+const Results = (props) => {
+  if (!props.results) return null;
+
+  if (props.results.length > 10)
+    return (
+      <>
+        <br></br>Too many matches, specify another filter
+      </>
+    );
+
+  if (props.results.length === 1) {
+    const country = props.results[0];
+    console.log(country);
+    return (
+      <div>
+        <h1>{country.name.common}</h1>
+        <p>
+          capital {country.capital[0]}
+          <br></br>
+          area {country.area}
+        </p>
+        <h3>languages</h3>
+        <ul>
+          {Object.values(country.languages).map((lang) => (
+            <li key={lang}>{lang}</li>
+          ))}
+        </ul>
+        <img src={country.flags.png} alt={country.flags.alt} />
+      </div>
+    );
+  }
+
+  console.log("rendering");
+  return (
+    <div>
+      {props.results.map((result) => {
+        return <div key={result.name.common}>{result.name.common}</div>;
+      })}
+    </div>
+  );
+};
+
+export default App;

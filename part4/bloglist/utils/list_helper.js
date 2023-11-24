9c96989a0bcd6e0c19a5e4d851a96d297684f6ba
MooCodes <60076255+MooCodes@users.noreply.github.com>
2023-11-24 13:25:06 -0600

    4.5: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -8,7 +8,26 @@ const totalLikes = (blogs) => {
   return blogs.reduce((acc, curr) => curr.likes + acc, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  const highestLikedBlog = blogs.reduce((prev, curr) => {
+    if (prev.likes > curr.likes)
+      return {
+        title: prev.title,
+        author: prev.author,
+        likes: prev.likes,
+      };
+    return {
+      title: curr.title,
+      author: curr.author,
+      likes: curr.likes,
+    };
+  });
+
+  return highestLikedBlog;
+};
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 };

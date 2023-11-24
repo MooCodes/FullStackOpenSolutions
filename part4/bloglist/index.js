7dd4cb24de70f811cc7cd6f3d243034c27f3ca6e
MooCodes <60076255+MooCodes@users.noreply.github.com>
2023-11-24 12:05:54 -0600

    4.2 Blog list, step2

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,41 +1,31 @@
-require("dotenv").config();
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require("./utils/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const blogsRouter = require("./controllers/blogs");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    logger.info("Connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("Error connecting to MongoDB", error.message);
+  });
 
-const Blog = mongoose.model('Blog', blogSchema)
-mongoose.connect(process.env.MONGODB_URI)
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
 
-app.use(cors())
-app.use(express.json())
+app.use("/api/blogs", blogsRouter);
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});

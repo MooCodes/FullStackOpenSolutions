e1b85fe3d4b4c0bcbf974aae0070754b033eeb8b
MooCodes <60076255+MooCodes@users.noreply.github.com>
2023-11-25 18:26:10 -0600

    4.11 Blog list tests, step4

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -55,8 +55,29 @@ test("add a new blog", async () => {
   const blogsAtEnd = await helper.blogsInDb();
   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
 
-  const titles = blogsAtEnd.map(blog => blog.title);
+  const titles = blogsAtEnd.map((blog) => blog.title);
   expect(titles).toContain("new blog test");
+});
+
+test("likes property missing defaults to 0", async () => {
+  const newBlog = {
+    title: "new blog test",
+    author: "Ali Baig",
+    url: "someRandomURL.com",
+  };
+
+  await api
+    .post("/api/blogs")
+    .send(newBlog)
+    .expect(201)
+    .expect("Content-Type", /application\/json/);
+
+  const blogsAtEnd = await helper.blogsInDb();
+  const missingLikesBlog = blogsAtEnd.find(
+    (blog) => blog.title === "new blog test"
+  );
+
+  expect(missingLikesBlog.likes).toBe(0);
 });
 
 afterAll(async () => {

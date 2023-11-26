e50bd8f74924adbc12dac85d4494870ecb8913dc
MooCodes <60076255+MooCodes@users.noreply.github.com>
2023-11-25 19:20:52 -0600

    added async/await + error handling to blogs router

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,21 +1,24 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
+blogsRouter.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
 });
 
-blogsRouter.post("/", (request, response, next) => {
+blogsRouter.post("/", async (request, response, next) => {
   const blog = new Blog(request.body);
 
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  }).catch((error) => {
-    response.status(400)
-    next(error);
-  });
+  // try {
+  //   const savedBlog = await blog.save();
+  //   response.status(201).json(savedBlog);
+  // } catch (exception) {
+  //   next(exception);
+  // }
+
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
+
 });
 
 module.exports = blogsRouter;

e77feca22b38716234961278a1e4e34bab79bd65
MooCodes <60076255+MooCodes@users.noreply.github.com>
2023-11-27 14:49:01 -0600

    4.17: bloglist expansion, step5

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,22 +1,36 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
+const User = require("../models/user");
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
+  const blogs = await Blog.find({}).populate("user", {
+    username: 1,
+    name: 1,
+    id: 1,
+  });
   response.json(blogs);
 });
 
 blogsRouter.post("/", async (request, response, next) => {
-  const blog = new Blog(request.body);
+  const body = request.body;
 
-  // try {
-  //   const savedBlog = await blog.save();
-  //   response.status(201).json(savedBlog);
-  // } catch (exception) {
-  //   next(exception);
-  // }
+  const users = await User.find({}).limit(1);
+  const user = users[0];
+  console.log(user);
+
+  const newBlog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes,
+    user: user.id,
+  };
+
+  const blog = new Blog(newBlog);
 
   const savedBlog = await blog.save();
+  user.blogs = user.blogs.concat(savedBlog._id);
+  await user.save();
   response.status(201).json(savedBlog);
 });
 

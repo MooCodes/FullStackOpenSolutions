b098b190fe2d6b87519cfce6708cd9013361fb0d
MooCodes <60076255+MooCodes@users.noreply.github.com>
2023-12-04 13:01:03 -0600

    5.4: bloglist frontend, step4

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import Blog from "./components/Blog";
 import blogService from "./services/blogs";
 import loginService from "./services/login";
+import "./App.css";
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
@@ -11,6 +12,7 @@ const App = () => {
   const [title, setTitle] = useState("");
   const [author, setAuthor] = useState("");
   const [url, setUrl] = useState("");
+  const [msg, setMsg] = useState(null);
 
   const fetchBlogs = async () => {
     const loggedUserJSON = window.localStorage.getItem("loggedBlogappUser");
@@ -56,6 +58,17 @@ const App = () => {
       setPassword("");
     } catch (exception) {
       console.log("error logging in with", username, password);
+
+      const newState = {
+        msg: "wrong username or password",
+        style: "error",
+      };
+
+      setMsg(newState);
+
+      setTimeout(() => {
+        setMsg(null);
+      }, 3000);
     }
   };
 
@@ -76,47 +89,82 @@ const App = () => {
       url: url,
     };
 
+    const newState = {
+      msg: `a new blog ${title} by ${author}`,
+      style: "success",
+    };
+
     const response = await blogService.create(blogToAdd);
     //const seperateBlog = blogs.concat(response)
     setBlogs(blogs.concat(response));
     setTitle("");
     setAuthor("");
     setUrl("");
+
+    setMsg(newState);
+
+    setTimeout(() => {
+      setMsg(null);
+    }, 3000);
   };
 
   if (user === null) {
-    return (
-      <form onSubmit={handleLogin}>
-        <div>
-          username
-          <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-          <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>
-    );
+    if (msg !== null) {
+      return (
+        <form onSubmit={handleLogin}>
+          <div className={msg.style}>{msg.msg}</div>
+          <div>
+            username
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      );
+    } else {
+      return (
+        <form onSubmit={handleLogin}>
+          <div>
+            username
+            <input
+              type="text"
+              value={username}
+              name="Username"
+              onChange={({ target }) => setUsername(target.value)}
+            />
+          </div>
+          <div>
+            password
+            <input
+              type="password"
+              value={password}
+              name="Password"
+              onChange={({ target }) => setPassword(target.value)}
+            />
+          </div>
+          <button type="submit">login</button>
+        </form>
+      );
+    }
   }
-
-  // const filteredBlogs = blogs.filter(
-  //   (blog) => blog.user.username === user.username
-  // );
 
   return (
     <div>
       <h2>blogs</h2>
+      {msg !== null && <div className={msg.style}>{msg.msg}</div>}
       <p>
         {user.name} logged in <button onClick={handleLogout}>logout</button>
       </p>

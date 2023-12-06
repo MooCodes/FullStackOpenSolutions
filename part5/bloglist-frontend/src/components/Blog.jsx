7e8b2c53238639b8651b6cad939faaa43f975db1
MooCodes <60076255+MooCodes@users.noreply.github.com>
2023-12-05 23:18:51 -0600

    5.7 Blog list frontend, step7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,39 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showMore, setShowMore] = useState(false);
+
+  const buttonText = !showMore ? "view" : "hide";
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  console.log(blog);
+
+  const blogDetails = () => {
+    return (
+      <div>
+        {blog.url}
+        <br></br>
+        {blog.likes} <button>like</button>
+        <br></br>
+        {blog.user.name}
+        <br></br>
+      </div>
+    );
+  };
+
+  return (
+    <div style={blogStyle}>
+      {blog.title} {blog.author} <button onClick={() => setShowMore(!showMore)}>{buttonText}</button>
+      {showMore && blogDetails()}
+    </div>
+  );
+};
+
+export default Blog;

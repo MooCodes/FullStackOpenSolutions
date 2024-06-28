e4f3a3413f8ad8d86b4e5e353604d0d7b17ba282
Ali Baig <alibaigsoftware@gmail.com>
2024-06-27 23:16:22 -0500

    9.5 WebBMI

--- part9/partb/bmiCalculator.ts
+++ part9/partb/bmiCalculator.ts
@@ -8,16 +8,4 @@ const calculateBmi = (height: number, weight: number): string => {
   else return "Obese";
 };
 
-const height = Number(process.argv[2]);
-const weight = Number(process.argv[3]);
-
-if (isNaN(height) || isNaN(weight)) {
-  throw new Error("Provided values were not numbers!");
-}
-
-if (height < 0 || weight < 0) {
-  throw new Error("Provided values were not positive numbers!");
-}
-
-console.log(calculateBmi(height, weight));
-
+export default calculateBmi;
--- part9/partb/index.ts
+++ part9/partb/index.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import calculateBmi from "./bmiCalculator";
 
 const app = express();
 
 app.get("/hello", (_req, res) => {
   res.send("Hello Full Stack!");
+});
+
+app.get("/bmi", (_req, res) => {
+  const height = Number(_req.query.height);
+  const weight = Number(_req.query.weight);
+
+  if (isNaN(height) || isNaN(weight))
+    res.json({ error: "malformatted parameters" });
+  else res.json({ weight, height, bmi: calculateBmi(height, weight) });
 });
 
 app.listen(3003, () => {

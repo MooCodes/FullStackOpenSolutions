455d6b13bd8a3cc0bda0cfda8a32eace9d45cfd6
Ali Baig <alibaigsoftware@gmail.com>
2024-06-27 19:18:06 -0500

    9.2 Exercise calculator

---
+++ part9/partb/exerciseCalculator.ts
@@ -0,0 +1,34 @@
+interface Results {
+  numOfDays: number;
+  numOfTrainingDays: number;
+  target: number;
+  average: number;
+  success: boolean;
+  rating: 1 | 2 | 3;
+  ratingDescription: string;
+}
+const calculateExercises = (target: number, exercises: number[]): Results => {
+  const numOfDays = exercises.length;
+  const numOfTrainingDays = exercises.filter((x) => x > 0).length;
+  const average =
+    exercises.reduce((acc, curr) => acc + curr, 0) / exercises.length;
+  const success = average >= target;
+  const rating = success ? 3 : average > target / 2 ? 2 : 1;
+  const ratingDescription = success
+    ? "good"
+    : average > target / 2
+    ? "not too bad but could be better"
+    : "bad";
+
+  return {
+    numOfDays,
+    numOfTrainingDays,
+    target,
+    average,
+    success,
+    rating,
+    ratingDescription,
+  };
+};
+
+console.log(calculateExercises(2, [3, 0, 2, 4.5, 0, 3, 1]));

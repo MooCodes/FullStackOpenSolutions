e336ed973a839e351d073ec31791e5461b1a09e1
Ali Baig <alibaigsoftware@gmail.com>
2024-06-29 19:59:18 -0500

    Exercise 9.17

--- part9/diary-frontend/src/App.tsx
+++ part9/diary-frontend/src/App.tsx
@@ -9,35 +9,100 @@ interface Diary {
   comment: string;
 }
 
+type DiaryEntry = Omit<Diary, "id">;
+
 const getAllDiaries = async () => {
-  const { data } = await axios.get<Diary[]>("http://localhost:3000/api/diaries");
+  const { data } = await axios.get<Diary[]>(
+    "http://localhost:3000/api/diaries"
+  );
+  return data;
+};
+
+const createDiary = async (entry: DiaryEntry) => {
+  const { data } = await axios.post<Diary>(
+    "http://localhost:3000/api/diaries",
+    entry
+  );
   return data;
 };
 
 function App() {
   const [diaries, setDiaries] = useState<Diary[]>([]);
+  const [entry, setEntry] = useState<DiaryEntry>({
+    date: "",
+    weather: "",
+    visibility: "",
+    comment: "",
+  });
 
   useEffect(() => {
     getAllDiaries().then((data) => setDiaries(data));
   }, []);
 
-  if (diaries.length === 0) {
-    return <>Loading...</>;
-  }
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    createDiary(entry).then((data) => setDiaries([...diaries, data]));
+  };
 
-  console.log(diaries);
+  if (diaries.length === 0) return <>Loading...</>;
 
-  return <>
-    <h2>Diary Entries</h2>
-    {diaries.map((diary) => (
-      <div key={diary.id}>
-        <h3>{diary.date}</h3>
-        weather: {diary.weather}<br />
-        visiblity: {diary.visibility}<br />
-        comment: {diary.comment}
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <h2>Add New Entry</h2>
+        <div>
+          date:{" "}
+          <input
+            value={entry.date}
+            type="text"
+            name="date"
+            onChange={(e) => setEntry({ ...entry, date: e.target.value })}
+          />
+        </div>
+        <div>
+          weather:{" "}
+          <input
+            value={entry.weather}
+            type="text"
+            name="weather"
+            onChange={(e) => setEntry({ ...entry, weather: e.target.value })}
+          />
+        </div>
+        <div>
+          visibility:{" "}
+          <input
+            value={entry.visibility}
+            type="text"
+            name="visibility"
+            onChange={(e) => setEntry({ ...entry, visibility: e.target.value })}
+          />
+        </div>
+        <div>
+          comment:{" "}
+          <input
+            value={entry.comment}
+            type="text"
+            name="comment"
+            onChange={(e) => setEntry({ ...entry, comment: e.target.value })}
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
+      <h2>Diary Entries</h2>
+      <div>
+        {diaries.map((diary) => (
+          <div key={diary.id}>
+            <h3>{diary.date}</h3>
+            weather: {diary.weather}
+            <br />
+            visiblity: {diary.visibility}
+            <br />
+            comment: {diary.comment}
+          </div>
+        ))}
       </div>
-    ))}
-  </>;
+    </>
+  );
 }
 
 export default App;
